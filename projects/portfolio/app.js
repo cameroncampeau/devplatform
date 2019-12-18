64de17fbded4af3e7b7282e4601c1bc48c2d21af
ccampeau@cryptomill.com <ccampeau@cryptomill.com>
2019-12-18 15:22:27 -0500

    A

--- projects/portfolio/app.js
+++ projects/portfolio/app.js
@@ -6,5 +6,8 @@ route.use("/dist", express.static(path.resolve(__dirname + "/public/dist")))
 route.get("/", (req,res) => {
     res.sendFile(path.resolve(__dirname + "/public/index.html"))
 })
+route.get("/projects", (req,res) => {
+    res.sendFile(path.resolve(__dirname + "/data/projects.json"))
+});
 
 module.exports = route;
