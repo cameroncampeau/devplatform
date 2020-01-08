01b80f9b78eec411655c766247dcca8c27759925
cameron <cameron.campeau@hotmail.com>
2020-01-07 23:46:49 -0500

    A

--- projects/projectredirect/app.js
+++ projects/projectredirect/app.js
@@ -11,7 +11,7 @@ router.post("/redirect", (req,res) => {
     var {username, password, name} = req.body;
     if (!username || !password || !name) return res.end();
     if (username != USERNAME || password != PASSWORD) return res.end();
-    var ip = req.ip;
+    var ip = req.ip.split(":").pop();
     db.getCollection(COLLECTION_NAME).remove({name});
     db.saveItem(COLLECTION_NAME, {name, ip});
     res.send({name, ip})
