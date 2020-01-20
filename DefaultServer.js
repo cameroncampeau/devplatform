3d1a8bd3a0d42ebbf8bf0e3a9304c786914bcf91
ccampeau@cryptomill.com <ccampeau@cryptomill.com>
2020-01-20 12:51:03 -0500

    https

--- DefaultServer.js
+++ DefaultServer.js
@@ -1,14 +1,26 @@
 var express = require("express"),
-  app = express();
+	app = express(),
+	https = require("https"),
+	fs = require("fs").promises;
 
 function addRoute(path, router) {
-  app.use(path, router);
+	app.use(path, router);
 }
 
-function start(port) {
-  port = port || 80;
-  app.listen(port);
-  console.log("Starting default server on port", port);
+async function start(port) {
+	var key = await fs.readFile("./certs/privkey.pem");
+	var cert = await fs.readFile("./certs/fullchain.pem");
+	port = port || 443;
+	https
+		.createServer(
+			{
+				key,
+				cert
+			},
+			app
+		)
+		.listen(port);
+	console.log("Starting default server on port", port);
 }
 
 module.exports = { addRoute, start };
--- app.js
+++ app.js
@@ -1,41 +1,43 @@
 var fs = require("fs"),
-  path = require("path"),
-  PortTracker = require("./PortManager"),
-  DefaultServer = require("./DefaultServer");
+	path = require("path"),
+	PortTracker = require("./PortManager"),
+	DefaultServer = require("./DefaultServer"),
+	process = require("process");
 
 var projects = [];
 function print_port_usage() {
-  console.log("Ports in use:", PortTracker.getUsedPorts());
+	console.log("Ports in use:", PortTracker.getUsedPorts());
 }
 function loadProject(folder) {
-  var folderPath = path.resolve(__dirname + "/projects/" + folder);
-  var project_files = fs.readdirSync(folderPath);
-  if (!project_files.find(f => f == "project_config.js")) return;
-  var config = require(folderPath + "/project_config.js");
-  projects.push({ folder, config });
-  config.load();
+	var folderPath = path.resolve(__dirname + "/projects/" + folder);
+	var project_files = fs.readdirSync(folderPath);
+	if (!project_files.find(f => f == "project_config.js")) return;
+	var config = require(folderPath + "/project_config.js");
+	projects.push({ folder, config });
+	if (config.load) config.load();
 }
 function load_projects() {
-  function isFolder(fileName) {
-    return fs
-      .lstatSync(path.resolve(__dirname + "/projects") + "/" + fileName)
-      .isDirectory();
-  }
-  var project_folders = fs.readdirSync(path.resolve(__dirname + "/projects"));
+	function isFolder(fileName) {
+		return fs
+			.lstatSync(path.resolve(__dirname + "/projects") + "/" + fileName)
+			.isDirectory();
+	}
+	if (process.argv.length > 2) return loadProject(process.argv[2]);
+	var project_folders = fs.readdirSync(path.resolve(__dirname + "/projects"));
 
-  project_folders.forEach(project_folder => {
-    try {
-      if (isFolder(project_folder)) loadProject(project_folder);
-    } catch (e) {
-      console.error("Error loading project", project_folder);
-      console.error(e);
-    }
-  });
+	project_folders.forEach(project_folder => {
+		try {
+			if (isFolder(project_folder)) loadProject(project_folder);
+		} catch (e) {
+			console.error("Error loading project", project_folder);
+			console.error(e);
+		}
+	});
 }
 
 function init() {
-  DefaultServer.start();
-  load_projects();
-  print_port_usage();
+	DefaultServer.start();
+	load_projects();
+	print_port_usage();
 }
 init();
