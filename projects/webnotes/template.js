ed8367f86ed22f769bc70851fc95eccf57e7b53d
cameron <cameron.campeau@hotmail.com>
2020-02-19 16:39:10 -0500

    A

--- projects/webnotes/template.js
+++ projects/webnotes/template.js
@@ -15,6 +15,7 @@ module.exports.get = module.exports.build = markdown => `<html>
 />
 <body>
     <div id="markdown" class="my-md-5 container p-4 border rounded"></div>
+    <code id="text" style="display:none">${markdown}</code>
     <script
         src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.8.0/marked.min.js"
         integrity="sha256-fIsvyYkI1qQV1tu1PdlM4FF43kW76YCnCcl/xJCD3Fc="
@@ -24,7 +25,7 @@ module.exports.get = module.exports.build = markdown => `<html>
     window.onload = function() {
         var $container = document.querySelector("#markdown");
     
-        $container.innerHTML = marked(\`${markdown}\`);
+        $container.innerHTML = marked(document.querySelector("#text").innerText);
     }
     </script>
     </body>
