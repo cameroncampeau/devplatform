4643aeed09c331601eade44f516a3e041cf3bc12
cameron <cameron.campeau@hotmail.com>
2020-02-19 16:33:53 -0500

    A

--- projects/webnotes/template.js
+++ projects/webnotes/template.js
@@ -14,7 +14,7 @@ module.exports.get = module.exports.build = markdown => `<html>
     crossorigin="anonymous"
 />
 <body>
-    <div id="markdown" class="my-md-5 container p-4 border rounded">${markdown}</div>
+    <div id="markdown" class="my-md-5 container p-4 border rounded"></div>
     <script
         src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.8.0/marked.min.js"
         integrity="sha256-fIsvyYkI1qQV1tu1PdlM4FF43kW76YCnCcl/xJCD3Fc="
@@ -24,7 +24,7 @@ module.exports.get = module.exports.build = markdown => `<html>
     window.onload = function() {
         var $container = document.querySelector("#markdown");
     
-        $container.innerHTML = marked($container.innerHTML);
+        $container.innerHTML = marked(\`${markdown}\`);
     }
     </script>
     </body>
